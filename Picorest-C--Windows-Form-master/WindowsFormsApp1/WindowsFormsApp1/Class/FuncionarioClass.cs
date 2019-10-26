60134739cbf8fb125a6205a65c3b1f74f7b5f5ba
Emanuel Augusto <emanuel02augusto@gmail.com>
2019-10-26 18:29:28 -0300

    Cabecalhos

--- Picorest-C--Windows-Form-master/WindowsFormsApp1/WindowsFormsApp1/Class/FuncionarioClass.cs
+++ Picorest-C--Windows-Form-master/WindowsFormsApp1/WindowsFormsApp1/Class/FuncionarioClass.cs
@@ -17,16 +17,38 @@ namespace WindowsFormsApp1.Class
 
         [DisplayName("Data de Admissão")]
         public string datadeAdmissao { get; set; }
+
+        [DisplayName("Rg")]
         public string rg { get; set; }
+
+        [DisplayName("Cpf")]
         public string cpf { get; set; }
+
+        [DisplayName("Endereço")]
         public string endereco { get; set; }
+
+        [DisplayName("E-mail")]
         public string email { get; set; }
+
+        [DisplayName("Carteira de Habilitação")]
         public string cnh { get; set; }
+
+        [DisplayName("função")]
         public string funcao { get; set; }
+
+        [DisplayName("Setor")]
         public string setor { get; set;}
+
+        [DisplayName("Cursos")]
         public string cursos { get; set; }
+
+        [DisplayName("Nome da Instituição")]
         public string nomeInstituicao { get; set; }
+
+        [DisplayName("Nome de usuário")]
         public string nomeUsuario { get; set; }
+
+        [DisplayName("Senha")]
         public string senha { get; set; }
     }
 
